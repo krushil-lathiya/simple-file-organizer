6db4b4a595a1f59380ab8677b7d2410f28716e5f
krushil_lathiya <krushillathiya1211@gmail.com>
2025-05-06 16:45:36 +0200

    Add function to organize organize image files

--- file_organizer.py
+++ file_organizer.py
@@ -1,1 +1,18 @@
-dint do it okkk????
+import os
+import shutil
+from pathlib import Path
+
+# Define the folder you want to organize
+# Change this to any folder you want to organize
+DOWNLOADS_FOLDER = Path.home() / "AppData//Local//Programs//data"
+# DOWNLOADS_FOLDER = Path.home() 
+print("this is path :   ", DOWNLOADS_FOLDER)
+
+# Define the folder categories and associated file types
+FILE_TYPES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Videos": [".mp4", ".mov", ".mkv"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx"],
+    "Music": [".mp3", ".wav"],
+    "Archives": [".zip", ".rar", ".tar"],
+}
